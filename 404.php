e7df90c2454aace02543ba8d1beda918ac54f679
IceSlam <mainguy@mail.ru>
2020-12-09 17:17:11 +0300

    Added Error 404 page in theme template

--- 404.php
+++ 404.php
@@ -7,54 +7,44 @@
  * @package Вспоминая_будущее
  */
 
-get_header();
-?>
+ get_header();
+ ?>
 
-	<main id="primary" class="site-main">
+ <div class="container mt-5 heading">
+ 	<div class="row">
+ 		<div class="col-md-12">
+ 			<nav aria-label="breadcrumb">
+ 				<?php
+ 				 if ( function_exists('yoast_breadcrumb') ) {
+ 					 yoast_breadcrumb( '<ol class="breadcrumb">','</ol>' );
+ 				 }
+ 				 ?>
+ 			</nav>
+ 		</div>
+ 	</div>
+ 	<div class="row">
+ 		<div class="col-md-12">
+ 			<h2 class="page-title">
+				Ошибка 404. Страница не найдена
+ 			</h2>
+ 		</div>
+ 	</div>
+ </div>
+ </div>
+ </header>
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'remember' ); ?></h1>
-			</header><!-- .page-header -->
+ <main class="is-main">
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'remember' ); ?></p>
+ <section class="section page-info container mt-5 pt-5 pb-5 mb-5">
+ 	<div class="row">
+		<div class="col-md-12 pt-3">
+ 				<?php esc_html_e( 'К сожалению, страница не может быть найдена', 'remember' ); ?>
+		</div>
+ 		<div class="col-md-12 pb-3">
+ 			<?php esc_html_e( 'Похоже, что в этом месте ничего нет. Попробуйте задать поисковый запрос по-другому!', 'remember' ); ?>
+ 		</div>
+ 	</div>
+ </section>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'remember' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$remember_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'remember' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$remember_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
-
-<?php
-get_footer();
+ <?php
+ get_footer();
