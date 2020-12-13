7cf62a49b83cbfe660492081c642fe0a4890e31a
IceSlam <mainguy@mail.ru>
2020-12-14 01:18:48 +0300

    Refactored mobile menu in theme template

--- header.php
+++ header.php
@@ -115,14 +115,26 @@
 		              ) );
 		            ?>
 
-								<form class="d-flex input-group w-auto">
-									<button class="btn is-btn search" type="button" >
-										<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<path fill-rule="evenodd" clip-rule="evenodd" d="M2 8C2 4.691 4.691 2 8 2C11.309 2 14 4.691 14 8C14 11.309 11.309 14 8 14C4.691 14 2 11.309 2 8ZM17.707 16.293L14.312 12.897C15.365 11.543 16 9.846 16 8C16 3.589 12.411 0 8 0C3.589 0 0 3.589 0 8C0 12.411 3.589 16 8 16C9.846 16 11.543 15.365 12.897 14.312L16.293 17.707C16.488 17.902 16.744 18 17 18C17.256 18 17.512 17.902 17.707 17.707C18.098 17.316 18.098 16.684 17.707 16.293Z" fill="#70C8D3"/>
-										</svg>
-									</button>
+								<form class="d-flex input-group w-auto search-wish">
+									<div class="dropdown mx-auto mb-4">
+										<button
+											class="btn is-btn search"
+											type="button"
+											id="dropdownMenuButton"
+											data-toggle="dropdown"
+											aria-expanded="false"
+										>
+											<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path fill-rule="evenodd" clip-rule="evenodd" d="M2 8C2 4.691 4.691 2 8 2C11.309 2 14 4.691 14 8C14 11.309 11.309 14 8 14C4.691 14 2 11.309 2 8ZM17.707 16.293L14.312 12.897C15.365 11.543 16 9.846 16 8C16 3.589 12.411 0 8 0C3.589 0 0 3.589 0 8C0 12.411 3.589 16 8 16C9.846 16 11.543 15.365 12.897 14.312L16.293 17.707C16.488 17.902 16.744 18 17 18C17.256 18 17.512 17.902 17.707 17.707C18.098 17.316 18.098 16.684 17.707 16.293Z" fill="#70C8D3"/>
+											</svg>
+										</button>
+									  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+									    <?php get_search_form(); ?>
+									  </ul>
+									</div>
+
 									<a
-										class="btn is-btn wish mt-3"
+										class="btn is-btn wish"
 										href="<? echo get_category_link(3); ?>"
 									>
 										Осуществить желание
